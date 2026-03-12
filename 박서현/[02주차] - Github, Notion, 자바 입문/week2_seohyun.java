51825c13db4d3421a174f9cb5d399d0d0133b28e
shpark0305 <raonhajea0305@gmail.com>
2026-03-12 15:10:45 +0900

    2강 수강완료, 기본적인 자바 구조 학습

---
+++ 박서현/[02주차] - Github, Notion, 자바 입문/week2_seohyun.java
@@ -0,0 +1,18 @@
+
+/*
+1. Hello World 프린트
+-파일명과 클래스의 이름은 같아야한다
+-C와 똑같이 세미콬ㄹ론으로 문장을 구분하기에 문장이 끝나면 세미콜론 필수
+
+ */
+
+public class week2_seohyun {
+
+
+    public static void main(String[] args){
+        System.out.println("hello java");
+        System.out.println("hello java1");
+        // System.out.println("hello java2"); 주석처리된 코드는 실행안됨
+        System.out.println("hello java3");
+    }
+}

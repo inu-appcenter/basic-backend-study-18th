c36f07a1008d3dfe4edb080f03cc2e6f13be798b
Dweb <inu@inu.ac.kr>
2026-03-13 22:36:57 +0900

    week2/박건희_section2

---
+++ 박건희/[02주차] - Github, Notion, 자바 입문/prac/Section2/variable_5.java
@@ -0,0 +1,26 @@
+package Section2;
+// 변수 문제와 풀이
+public class variable_5 {
+    static void main() {
+
+        // Q1.
+        int num1 = 10, num2 = 5;
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+
+        // Q2.
+        int num3 = 10, num4 = 20;
+        int sum = num3 + num4;
+        System.out.println(sum);
+
+        // Q3.
+        long longVar = 10000000000L;
+        System.out.println(longVar);
+
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+
+    }
+}

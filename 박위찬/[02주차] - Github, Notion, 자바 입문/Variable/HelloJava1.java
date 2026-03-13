15e4564882bdd6642c605d95d52b3ea07d0a2f35
박위찬 <01046883774q@gmail.com>
2026-03-13 15:09:51 +0900

    1,2강 코드 실습 내용

--- 박위찬/[02주차] - Github, Notion, 자바 입문/Variable/HelloJava1.java
+++ 박위찬/[02주차] - Github, Notion, 자바 입문/Variable/HelloJava1.java
@@ -1,6 +1,9 @@
-public class HelloJava {
+package Variable;
+
+//1강
+public class HelloJava1 {
     public static void main(String[] args){
-        System.out.println("Hllow");
+        System.out.println("Hellow");
         /*System.out.println("Java"); //hellow
         System.out.println("Hellow Java2"); //주석*/
         System.out.println("안녕"); // sout 치면 자동으로 만들어줌
---
+++ 박위찬/[02주차] - Github, Notion, 자바 입문/Variable/ex/HelloJava2_ex1.java
@@ -0,0 +1,10 @@
+package Variable.ex;
+
+public class HelloJava2_ex1 {
+    public static void main(String[] args){
+        int num1=4, num2=3;
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+    }
+}

801bd0150018b85fb2e14baeb91109ad3dd95fec
Dweb <inu@inu.ac.kr>
2026-03-14 00:15:59 +0900

    week2/박건희_section3

---
+++ 박건희/[02주차] - Github, Notion, 자바 입문/prac/Section3/operator_3.java
@@ -0,0 +1,18 @@
+package Section3;
+// 연산자 우선순위
+public class operator_3 {
+    public static void main(String[] args) {
+
+        int sum1 = 1 + 2 * 3; //1 + (2 * 3)과 같다.
+        int sum2 = (1 + 2) * 3;
+        System.out.println("sum1 = " + sum1);
+        System.out.println("sum2 = " + sum2);
+
+        int sum3 = 2 * 2 + 3 * 3;
+        int sum4 = (2 * 2) + (3 * 3); // sum3과 동일한 값을 가짐
+        System.out.println("sum3 = " + sum3);
+        System.out.println("sum4 = " + sum4);
+
+        // 연산자의 우선순위는 상식선의 연산을 생각하거나 헷갈린다면 괄호 활용하기
+    }
+}
---
+++ 박건희/[02주차] - Github, Notion, 자바 입문/prac/Section3/operator_7.java
@@ -0,0 +1,23 @@
+package Section3;
+//대입 연산자
+public class operator_7 {
+    static void main() {
+
+        int a = 5; // 5
+
+        //변수의 값을 변환 산술 연산자에 해당하는 연산 후 저장
+        a += 3;
+        System.out.println(a);
+        a -= 2;
+        System.out.println(a);
+        a *= 4;
+        System.out.println(a);
+        a /= 3;
+        System.out.println(a);
+        a %= 5;
+        System.out.println(a);
+
+
+
+    }
+}

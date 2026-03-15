677beee6c73abcf768fd172ef6b4a33059ae19a2
shpark0305 <raonhajea0305@gmail.com>
2026-03-15 16:39:23 +0900

    챕터 5 완료, 조건문

---
+++ 박서현/[02주차] - Github, Notion, 자바 입문/cond/ex/EvenOddEx.java
@@ -0,0 +1,21 @@
+package cond.ex;
+
+import java.util.Scanner;
+
+public class EvenOddEx {
+    public static void main(String[] args){
+        int x;
+        String eo;
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            System.out.print("x: ");
+            x=sc.nextInt();
+
+            if (x==-1) break;
+            eo = (x%2==1) ? "홀수" :"짝수";
+            System.out.println("x= "+x+ ", "+eo);
+
+
+    }
+}

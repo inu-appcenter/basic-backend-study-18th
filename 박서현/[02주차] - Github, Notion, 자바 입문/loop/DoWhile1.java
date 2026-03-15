3c4f213af5cd6edf4bad043311fe4d79668c0281
shpark0305 <raonhajea0305@gmail.com>
2026-03-15 17:05:27 +0900

    2주차 과제 완료

--- 박서현/[02주차] - Github, Notion, 자바 입문/loop/DoWhile1.java
+++ 박서현/[02주차] - Github, Notion, 자바 입문/loop/DoWhile1.java
@@ -3,7 +3,7 @@ package loop;
 public class DoWhile1 {
 
     public static void main(String[] args) {
-        int i = 10;
+        int i = 0;
         while (i < 3) {
             System.out.println("현재 숫자는:" + i);
             i++;

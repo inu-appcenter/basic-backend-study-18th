2825e48d7255c8fe751c1a8ae91828d1320dce89
Dweb <inu@inu.ac.kr>
2026-03-21 19:11:20 +0900

    week3/박건희_Section10

---
+++ 박건희/[03주차] - 자바 입문/prac/Section10/method_7.java
@@ -0,0 +1,16 @@
+package Section10;
+
+public class method_7 {
+
+    public static void main(String[] args) {
+
+        int number = 100;
+        printNumber(number);
+
+    }
+
+    public static void printNumber(double n) {
+        System.out.println("숫자: " + n);
+    }
+
+}

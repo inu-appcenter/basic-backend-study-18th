f17e70da68ef25946bfdd643926f6338e00e3e1b
윤유진 <yj30421@inu.ac.kr>
2026-03-22 20:55:19 +0900

    [03주차]김영한의 자바 입문 섹션 7 ~ 완강 - 윤유진

---
+++ 윤유진/[03주차] - 자바 입문/scanner/Scanner1.java
@@ -0,0 +1,24 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Scanner1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("hello");
+        System.out.println("world");
+
+        System.out.print("문자열을 입력하세요: "); //println으로 할 경우 커서가 다음 줄로 넘어감
+        String str = scanner.nextLine(); //입력을 String으로 가져온다. 프로그램이 이 줄을 만나면 입력을 받을 때까지 기다림.
+        System.out.println("입력한 문자열: " + str);
+
+        System.out.print("정수를 입력하세요: ");
+        int intValue = scanner.nextInt();
+        System.out.println("입력한 정수: " + intValue);
+
+        System.out.print("실수를 입력하세요: ");
+        double doubleValue = scanner.nextDouble();
+        System.out.println("입력한 실수: " + doubleValue);
+    }
+}

750530a24de5c4ee45dab2a648efa00b885e8b84
Dweb <inu@inu.ac.kr>
2026-03-27 16:55:26 +0900

    week4/박건희_Section4

---
+++ 박건희/[04주차] - 자바 기본/prac/Section4/Objective_1.java
@@ -0,0 +1,33 @@
+package Section4;
+
+public class Objective_1 {
+
+    static void main() {
+
+        int volume = 0;
+        boolean isOn = false;
+
+        isOn = true;
+        System.out.println("음악 플레이어를 시작합니다");
+
+        volume++;
+        System.out.println("음악 플레이어 볼륨:" + volume);
+
+        volume++;
+        System.out.println("음악 플레이어 볼륨:" + volume);
+
+        volume--;
+        System.out.println("음악 플레이어 볼륨:" + volume);
+
+        System.out.println("음악 플레이어 상태 확인");
+        if (isOn) {
+            System.out.println("음악 플레이어 ON, 볼륨:" + volume);
+        } else {
+            System.out.println("음악 플레이어 OFF");
+        }
+
+        isOn = false;
+        System.out.println("음악 플레이어를 종료합니다");
+
+    }
+}

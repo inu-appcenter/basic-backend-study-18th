6ce68aef58b963f9e3b456d907cd166a1b0b2499
shpark0305 <raonhajea0305@gmail.com>
2026-03-29 00:36:12 +0900

    week4 완료

---
+++ 박서현/[04주차] - 자바 기본/pack/PackageMain3.java
@@ -0,0 +1,8 @@
+package pack;
+import pack.a.user;
+public class PackageMain3 {
+    public static void main(String[] args) {
+        user userA = new user();
+        pack.b.user userB = new pack.b.user();
+    }
+}

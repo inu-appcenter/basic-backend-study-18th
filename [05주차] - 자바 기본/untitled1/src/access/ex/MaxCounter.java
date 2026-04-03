0511ad961d3cb4ec44b80c3c26761bd8660dc03f
ldj <noveldj@naver.com>
2026-04-03 14:12:19 +0900

    5주차 과제입니다

---
+++ [05주차] - 자바 기본/untitled1/src/access/ex/MaxCounter.java
@@ -0,0 +1,20 @@
+package access.ex;
+
+public class MaxCounter {
+    private int count;
+    private int max;
+
+    public MaxCounter(int max) {
+        this.max = max;
+    }
+    public void increasement() {
+        if (count >= max) {
+            System.out.println("최댓값을 초과할 수 없습니다.");
+            return;
+        }
+        count++;
+    }
+    public int getCount() {
+        return count;
+    }
+}

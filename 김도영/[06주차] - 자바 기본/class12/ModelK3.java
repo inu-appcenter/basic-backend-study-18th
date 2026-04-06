11a77fc232abcaf4024c17f19bfb92d5eb257e3f
kimdoyoung1110 <from.dozero@gmail.com>
2026-04-06 19:19:59 +0900

    feat: 12 다형성 활용예제 1,2

---
+++ 김도영/[06주차] - 자바 기본/class12/ModelK3.java
@@ -0,0 +1,13 @@
+package class12;
+
+public class ModelK3 {
+    public void startEngine(){
+        System.out.println("modelk3 startEngine");
+    }
+    public void offEngine(){
+        System.out.println("modelk3 offEngine");
+    }
+    public void pressAccelerator(){
+        System.out.println("modelk3 pressAccelerator");
+    }
+}

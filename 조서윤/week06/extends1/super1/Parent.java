b4667f807770a1912deaee42a5931da0f956f11a
조서윤 <heu0303@naver.com>
2026-04-11 16:29:46 +0900

    6주차 과제 제출

---
+++ 조서윤/week06/extends1/super1/Parent.java
@@ -0,0 +1,9 @@
+package week06.extends1.super1;
+
+public class Parent {
+    public String value = "parent";
+
+    public void hello() {
+        System.out.println("Parent.hello");
+    }
+}
---
+++ 조서윤/week06/poly/ex2/Caw.java
@@ -0,0 +1,8 @@
+package week06.poly.ex2;
+
+public class Caw extends Animal {
+    @Override
+    public void sound() {
+        System.out.println("음매");
+    }
+}

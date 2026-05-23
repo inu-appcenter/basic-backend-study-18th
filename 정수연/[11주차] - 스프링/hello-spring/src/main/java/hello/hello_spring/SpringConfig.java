7818fff0fc50d286ec11be20df739af65273a48c
suyeon <stellajeong6@naver.com>
2026-05-23 15:38:40 +0900

    섹션8

--- 정수연/[11주차] - 스프링/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ 정수연/[11주차] - 스프링/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hello_spring;
 
+import hello.hello_spring.aop.TimeTraceAop;
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.*;
 import hello.hello_spring.service.MemberService;
@@ -40,6 +41,12 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
+
+    //TimeTraceAop에서 @Component를 안쓴다면 @Bean
+    //@Bean
+    //public TimeTraceAop timeTraceAop(){
+    //    return new TimeTraceAop();
+    //}
 
     /*
     @Bean
